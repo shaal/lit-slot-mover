d7963d616b7a8f99dbd24d08d4371e279cd724a5
Ofer Shaal <oshaal@phase2technology.com>
2021-11-07 00:00:53 -0500

    Fix all edge-cases

--- src/components/slot-mover.ts
+++ src/components/slot-mover.ts
@@ -27,47 +27,63 @@ export class SlotMover extends LitElement {
     this.removeEventListener('slotchange', this.onSlotChange);
   }
 
+// /*
   render() {
     return html`
       <div>
         <h3>First Slot:</h3>
-        <slot name="first"></slot>
-        <h3>Default Slot:</h3>
+        <slot name="first">default first slot</slot>
+        <h3>Unnamed Slot:</h3>
         <slot></slot>
         <h3>Third Slot:</h3>
-        <slot name="third"></slot>
+        <slot name="third">default third slot</slot>
         <h3>Fin</h3>
       </div>
     `;
   }
+// */
+
+/*
+  render() {
+    return html`
+      <slot></slot>
+    `;
+  }
+*/
 
   onSlotChange() {
+    console.time();
+    // Search for all slots provided by the Web Component.
     const slotsArray = this.renderRoot.querySelectorAll('slot');
-    const currentElement = this;
-    // Go through each slot shadowdom position
-    slotsArray.forEach(slot => {
-      // Fetch the corresponding slot in the lightDom
-      // Named slot should move to their corresponding named slot
-      if (slot.name) {
-        const slotLightDom = currentElement.querySelector("[slot=" + slot.name + "]");
-        slot.appendChild(slotLightDom);
-      }
-      // Default slot should move to their corresponding position
-      else {
-      // Get all content that doesn't have slot as attribute
-        const slotLightDomArray = Array.from(currentElement.children).filter(
-          node => {
-            return (!node.getAttributeNode('slot'))
-          }
-        )
 
-        // Move all default slot content to its corresponding position
-        slotLightDomArray.forEach(slotLightDom=>
-          {
-          slot.previousElementSibling.appendChild(slotLightDom);
+    // If any slots found
+    if (slotsArray.length > 0) {
+      const currentElement = this;
+      // Go through each slot shadow dom position
+      slotsArray.forEach(slot => {
+        // Process named slot
+        if (slot.name) {
+          // Fetch the corresponding named slot in the lightDom
+          const slotLightDom = currentElement.querySelector("[slot=" + slot.name + "]");
+          // Only if named slot found in light dom - move it into shadow DOM
+          if (slotLightDom) {
+            slot.before(slotLightDom);
           }
-        );
-      }
-    });
+        }
+        // Process default (unnamed) slot
+        else {
+          // Get all content that doesn't have slot as attribute
+          const slotLightDomArray = Array.from(currentElement.children).filter(node => {
+              return (!node.getAttributeNode('slot'));
+          });
+
+          // Move all unnamed slot content to the corresponding position in shadow DOM
+          slotLightDomArray.forEach(slotLightDom => {
+            slot.before(slotLightDom);
+          });
+        }
+      });
+    }
+    console.timeEnd();
   }
 }
